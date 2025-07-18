6efe619b2c718d88cf6c1e4e120935c5861a9014
Stergios Lantzos <sterlan.2000@gmail.com>
2025-07-18 16:16:55 +0300

    updates

--- BACKEND/API/controllers/controller.js
+++ BACKEND/API/controllers/controller.js
@@ -433,6 +433,8 @@ const processRequest = async (req, res, did, deviceID, ePassportId) => {
           const currentLocation = Array.isArray(estimatedLocation)
             ? estimatedLocation.join(' | ')
             : estimatedLocation;
+
+          console.log("ESTIMATED LOCATION (MOCK): ", currentLocation)
         
           const device = await DEVICE.findOne({ did: did, device_id: deviceID });
           // if (accessStatus === "ACCESS_RESTRICTED") {
