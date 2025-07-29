217bfe3dbec710b1a8e9eb9b4110ebc2d4409038
Stergios Lantzos <sterlan.2000@gmail.com>
2025-07-29 15:48:11 +0300

    updates

--- BACKEND/API/controllers/controller.js
+++ BACKEND/API/controllers/controller.js
@@ -982,6 +982,8 @@ exports.handleAuthCallback = async (req, res) => {
       { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
     );
 
+    console.log(response)
+
     /* If authentication is successful, proceed to decode the vp_token */
     if (response.status === 200 && response.statusText === 'OK') {
       try {
