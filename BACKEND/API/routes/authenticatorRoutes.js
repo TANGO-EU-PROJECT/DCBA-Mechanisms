230cd846278e71e840afc8cabe6652344aa44656
Stergios Lantzos <sterlan.2000@gmail.com>
2025-07-29 17:15:45 +0300

    updates

--- BACKEND/API/routes/authenticatorRoutes.js
+++ BACKEND/API/routes/authenticatorRoutes.js
@@ -17,7 +17,7 @@ router.post('/begin-session', controller.beginSession);
  * @desc    Handle authentication callback and retrieve the access token.
  * @access  Public
  */
-router.post('/auth-callback', controller.handleAuthCallback);
+router.get('/auth-callback', controller.handleAuthCallback);
 
 
 /** [3] DONE
