d73f3d1785c2ec4de414dba284c357006d426b78
Stergios Lantzos <sterlan.2000@gmail.com>
2025-10-20 14:35:15 +0300

    updating deployment for RIAS Premises

--- MONGO_DB/MODELS/ALERTS.js
+++ MONGO_DB/MODELS/ALERTS.js
@@ -13,15 +13,15 @@ const schema_opts = {
 
 // Allowed values for estimated_location (reuse from DEVICE schema)
 const possibleLocations = [
-  // 'PACKAGING_LINES',
-  // 'PERMITTED_AREA',
-  // 'SORTING_LINES_1_TO_3',
-  // 'SORTING_LINES_4_AND_5',
-  // 'SORTING_LINES_6_TO_8',
-  // 'WAREHOUSE',
-  'EKETA-OFFICE-204',
-  'EKETA-OFFICE-201',
-  'SERVER-ROOM',
+  'PACKAGING_LINES',
+  'PERMITTED_AREA',
+  'SORTING_LINES_1_TO_3',
+  'SORTING_LINES_4_AND_5',
+  'SORTING_LINES_6_TO_8',
+  'WAREHOUSE',
+  // 'EKETA-OFFICE-204',
+  // 'EKETA-OFFICE-201',
+  // 'SERVER-ROOM',
   'UNKNOWN'
 ];
 
